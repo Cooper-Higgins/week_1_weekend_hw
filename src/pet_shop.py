3696940ed358b7ac43c465b2c860ea74aa04710d
Cooper-Higgins <jamie@supplantree.com>
2023-03-24 16:41:30 +0000

    First two?

---
+++ src/pet_shop.py
@@ -0,0 +1,10 @@
+# WRITE YOUR FUNCTIONS HERE
+
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(total_cash):
+    return total_cash["admin"]["total_cash"]
+
+def get_pets_sold(sold):
+    return sold["admin"]["pets_sold"]
